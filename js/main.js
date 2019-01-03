d4eabbd97e4936e2e041303443fdf6aa8402164e
msynko <marina.synko@gmail.com>
2019-01-03 14:08:03 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 
+const lightswitch = document.querySelector('.switch');
+const light = document.querySelector('.light');
+const text = document.querySelector('h1')
 
+ lightswitch.addEventListener('click', function() {
+   if (lightswitch.className === 'switch off') {
+     lightswitch.className = 'switch on';
+     light.className = 'light';
+     text.innerText = 'It\'s so bright in here!';
+   }
+   else if (lightswitch.className === 'switch on') {
+     lightswitch.className = 'switch off';
+     light.className = 'dark';
+     text.innerText = 'Hey, who turned off the lights?';
+   }
+ });
